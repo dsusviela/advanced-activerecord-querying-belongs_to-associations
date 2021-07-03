51641a752e1ad868803e2f3e878a7671f687d0eb
dsusviela <d.susviela@gmail.com>
2021-07-03 14:49:06 -0300

    Add region class method and spec

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,3 +1,7 @@
 class Region < ActiveRecord::Base
   has_many :locations
+
+  def self.in_region(region_name)
+    where(name: region_name)
+  end
 end
--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,4 +1,12 @@
 require "spec_helper"
 
 describe Region do
+  describe ".in_region" do
+    let!(:region) { create(:region, name: "Montevideo") }
+    let!(:second_region) { create(:region, name: "Not-Montevideo") }
+
+    it "returns the region by name" do
+      expect(described_class.in_region("Montevideo")).to eq([region])
+    end
+  end
 end
